baffd72e34421df24976ece7963b7d5546059054
Alvaro Siesquén <alvaro.siesquen@unmsm.edu.pe>
2024-09-10 17:15:20 -0500

    fix: scraping all urls with a function

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from search import WebScrapingFalabella, WebScrapingSodimac
+from search import WebScraping
 from create_dataset import create_dataset
 
 url_falabella = 'https://www.falabella.com.pe/falabella-pe/search?Ntt='
 url_sodimac = 'https://www.sodimac.com.pe/sodimac-pe/buscar?Ntt='
 url_linio = 'https://linio.falabella.com.pe/linio-pe/search?Ntt='
-url_tottus = 'https://tottus.falabella.com.pe/tottus-pe/search?Ntt=honor+x8'
+url_tottus = 'https://tottus.falabella.com.pe/tottus-pe/search?Ntt='
 
 query = input('Input the search: ')
 query = query.strip().replace(' ', '+')
 
 url_falabella += query
 url_sodimac += query
+url_linio += query
+url_tottus += query
 
-falabella_data = WebScrapingFalabella(url_falabella)
-sodimac_data = WebScrapingSodimac(url_sodimac)
-
+falabella_data = WebScraping(url_falabella)
+sodimac_data = WebScraping(url_sodimac)
+linio_data = WebScraping(url_linio)
+tottus_data = WebScraping(url_tottus)
 
 df_falabella = create_dataset(falabella_data[-1], falabella_data[:-1])
 df_sodimac = create_dataset(sodimac_data[-1], sodimac_data[:-1])
+df_linio = create_dataset(linio_data[-1], linio_data[:-1])
+df_tottus = create_dataset(tottus_data[-1], tottus_data[:-1])
 
 df_falabella.to_excel('data_falabella.xlsx', index=False)
 df_sodimac.to_excel('data_sodimac.xlsx', index=False)
+df_falabella.to_excel('data_linio.xlsx', index=False)
+df_sodimac.to_excel('data_tottus.xlsx', index=False)
 
-print(df_falabella)
-print(df_sodimac)
+print(df_falabella.iloc[0])
+print()
+print(df_sodimac.iloc[0])
+print()
+print(df_linio.iloc[0])
+print()
+print(df_tottus.iloc[0])
+print()
--- search.py
+++ search.py
@@ -1,38 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-def WebScrapingFalabella(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-
-    pedido_obtenido = requests.get(url, headers=headers)
-    html_obtenido = pedido_obtenido.text
-    soup = BeautifulSoup(html_obtenido, 'html.parser')
-
-    title = soup.find('title').text
-
-    product_scraping = soup.find_all('b', {'class': 'jsx-3038137090'})
-    product_prices = soup.find_all('li', {'class': 'jsx-3604446019 prices-0'})
-
-    product_type, product_name, product_business = [], [], []
-    for i in range(len(product_scraping)):
-        if i%3 == 0:
-            product_type.append(product_scraping[i].text)
-        if i%3 == 1:
-            product_name.append(product_scraping[i].text)
-        if i%3 == 2:
-            product_business.append(product_scraping[i].text)
-
-    for i in range(len(product_prices)):
-        product_prices[i] = product_prices[i].text
-    
-    columns = ['Nombre', 'Precio', 'Tipo', 'Negocio']
-
-    return product_name, product_prices, product_type, product_business, columns
-
-
-def WebScrapingSodimac(url):
+def WebScraping(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
