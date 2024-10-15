8f5b986c14d0471c54486228d18cd1c4a03fe290
Alvaro Siesquén <alvaro.siesquen@unmsm.edu.pe>
2024-10-15 03:58:05 -0500

    Figma desing and CSS of index.html

--- create_dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def create_dataset(columns, data):
-    df = pd.DataFrame(columns=columns)
-    j = 0
-    for i in columns:
-        df[i] = data[j]
-        j += 1
-    return df
